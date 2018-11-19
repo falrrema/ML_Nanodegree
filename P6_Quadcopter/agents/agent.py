948c81ca6ce9bc1d6e0b92440cd340f9f54da9db
Fabián Reyes <falrrema@gmail.com>
2018-11-19 15:05:02 -0300

    improve reward and agent

--- P6_Quadcopter/agents/agent.py
+++ P6_Quadcopter/agents/agent.py
@@ -68,7 +68,7 @@ class Actor:
         # Add hidden layers
         net = layers.Dense(units=32, activation='relu')(states)
         net = layers.Dense(units=64, activation='relu')(net)
-        net = layers.Dense(units=32, activation='relu')(net)
+        net = layers.Dense(units=128, activation='relu')(net)
 
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
@@ -123,12 +123,12 @@ class Critic:
         actions = layers.Input(shape=(self.action_size,), name='actions')
 
         # Add hidden layer(s) for state pathway
-        net_states = layers.Dense(units=32, activation='relu')(states)
-        net_states = layers.Dense(units=64, activation='relu')(net_states)
+        net_states = layers.Dense(units=64, activation='relu')(states)
+        net_states = layers.Dense(units=128, activation='relu')(net_states)
 
         # Add hidden layer(s) for action pathway
-        net_actions = layers.Dense(units=32, activation='relu')(actions)
-        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+        net_actions = layers.Dense(units=64, activation='relu')(actions)
+        net_actions = layers.Dense(units=128, activation='relu')(net_actions)
 
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 

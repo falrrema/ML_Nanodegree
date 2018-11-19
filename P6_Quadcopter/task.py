15292595bac077e06f2774fda568612ea732cf57
Fabián Reyes <falrrema@gmail.com>
2018-11-19 10:22:46 -0300

    more changes to reward

--- P6_Quadcopter/task.py
+++ P6_Quadcopter/task.py
@@ -28,9 +28,12 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-       # reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        dist = np.linalg.norm(self.sim.pose[:3]-self.target_pos)
-        reward = 1 + 0.5*np.exp(-dist/100)
+        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        #reward = np.tanh(1 - 0.003*(abs(self.sim.pose[:3] - self.target_pos))).sum()
+        dist = np.linalg.norm(self.sim.pose[:3]-self.target_pos) # euclidean distance between the position and target
+        #variance = np.var(rotor_speeds)
+        reward = np.exp(-dist/(dist+100))
+        #+ (np.tanh(-self.sim.v[1]/500)) + (np.tanh(-self.sim.v[2]/500)) + (np.tanh(self.sim.v[3]/500))
         return reward
 
     def step(self, rotor_speeds):
